d892deb920fdd75f18c28171c11cf8ab726df632
Balasubramani R <46841670+balu8056@users.noreply.github.com>
2023-05-07 22:26:18 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body style="background-color: #242424">
+      <body style={{backgroundColor: "#242424"}}>
         <Main />
         <NextScript />
       </body>
