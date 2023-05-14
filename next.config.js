04718d88ac89ef5548b97f7bd96c2ec7b03efcf0
Balasubramani R <46841670+balu8056@users.noreply.github.com>
2023-05-14 11:55:51 +0530

    updated the next config for env

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 
 const nextConfig = {
   reactStrictMode: true,
-  env: process.env.NODE_ENV === 'development' ? undefined : dotenv.config().parsed,
+  env: dotenv.config().parsed,
 }
 
 module.exports = nextConfig
