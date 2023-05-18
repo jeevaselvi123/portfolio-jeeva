7969f162f57c5af0bc46b1cfd970310b0a677075
Jeeva Dharani Kamaraj <46011656+jeevaselvi123@users.noreply.github.com>
2023-05-18 23:02:36 +0530

    Updated footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <Typography color="#fff" textAlign="center" margin={1}>
       Copyright &copy; {new Date().getFullYear()} | Made with&nbsp;
       <FavoriteOutlinedIcon sx={{ width: '16px', height: '16px', fill: '#f43f5e' }} />
-      &nbsp;by Bala
+      &nbsp;by Jeeva
     </Typography>
   )
 }
