302eab648e64bee0675a8b702a8af52ba931ae25
Jeeva Dharani Kamaraj <46011656+jeevaselvi123@users.noreply.github.com>
2023-05-18 12:45:18 +0530

    Update index.tsx

--- src/components/SocialMediaIcon/index.tsx
+++ src/components/SocialMediaIcon/index.tsx
@@ -2,7 +2,7 @@ import TwitterIcon from '@mui/icons-material/Twitter'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import InstagramIcon from '@mui/icons-material/Instagram'
-
+import GitHubIcon from '@mui/icons-material/GitHub'
 const SocialMediaIcon = (name: string) => {
   return (
     <>
@@ -10,8 +10,8 @@ const SocialMediaIcon = (name: string) => {
       {name === 'LinkedIn' ? <LinkedInIcon /> : null}
       {name === 'Facebook' ? <FacebookIcon /> : null}
       {name === 'Instagram' ? <InstagramIcon /> : null}
+      {name === 'Github' ? <GitHubIcon /> : null}
     </>
   )
 }
-
 export default SocialMediaIcon
