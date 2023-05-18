18379804a484924f01be051cb1224fb0dac00823
Jeeva Dharani Kamaraj <46011656+jeevaselvi123@users.noreply.github.com>
2023-05-18 19:35:29 +0530

    Updated the link in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ const Header = () => (
     <title>Jeeva&#39;s Portfolio</title>
     <meta name="description" content="Personal Portfolio Web App" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link sx={{ borderRadius:'50%'}} rel="icon" href="/logo.png" />
+    <link rel="icon" href="/logo.png" />
   </Head>
 )
 
