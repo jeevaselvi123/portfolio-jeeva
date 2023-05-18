8530a2620830016a82790b17e0f28a5e3dc41cb9
Jeeva Dharani Kamaraj <46011656+jeevaselvi123@users.noreply.github.com>
2023-05-18 19:31:38 +0530

    Updated title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,10 @@ import Head from 'next/head'
 
 const Header = () => (
   <Head>
-    <title>Bala&#39;s Portfolio</title>
+    <title>Jeeva&#39;s Portfolio</title>
     <meta name="description" content="Personal Portfolio Web App" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/logo.png" />
+    <link sx={{ borderRadius:'50%'}} rel="icon" href="/logo.png" />
   </Head>
 )
 
